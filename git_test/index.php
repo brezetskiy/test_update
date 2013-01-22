51286e380a53adf66401a6897d033925e6f4d80d
brezetskiy <brezetskiy@brezetskiy-PC>
2013-01-22 19:01:02 +0200

    last?1

--- git_test/index.php
+++ git_test/index.php
@@ -9,7 +9,7 @@
             echo('test12');
             echo('some interesting example from notebook');
             
-            echo('no 2');
+            echo('no 3');
         ?>
     </body>
 </html>
