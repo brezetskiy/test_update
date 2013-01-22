0dcd3abc6f82bdd03a0ad73e7a6453b6a950a45f
brezetskiy <brezetskiy@brezetskiy-PC>
2013-01-22 18:49:23 +0200

    last?1

--- git_test/index.php
+++ git_test/index.php
@@ -9,7 +9,7 @@
             echo('test12');
             echo('some interesting example from notebook');
             
-            echo('no folders');
+            echo('no');
         ?>
     </body>
 </html>
